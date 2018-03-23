bbd2fe1b767acb6fcb9bf688a0d7de470ec9a31a
Seong Yong-ju <sei40kr@gmail.com>
2018-03-23 09:19:18 +0900

    Add packages.el
    Add packages.el with packages popwin, quickrun.

---
+++ packages.el
@@ -0,0 +1,38 @@
+;;; packages.el - quickrun layer file for Spacemacs
+;;
+;; Copyright (c) 2018 Seong Yong-ju
+;;
+;; Author: Seong Yong-ju <sei40kr@gmail.com>
+;; URL: https://github.com/sei40kr/dotfiles
+;;
+;; This file is not part of GNU Emacs
+;;
+;;; License: MIT
+
+(setq spacemacs-quickrun-packages
+      '(
+        popwin
+        quickrun))
+
+(defun spacemacs-quickrun/post-init-popwin ()
+  (push
+    '("*quickrun*"
+       :dedicated t
+       :position bottom
+       :stick t
+       :noselect nil
+       :height 0.4)
+    popwin:special-display-config))
+
+(defun spacemacs-quickrun/init-quickrun ()
+  (use-package quickrun
+    :defer t
+    :init
+    (progn
+      (setq quickrun-option-shebang t)
+      (spacemacs/declare-prefix "cq" "quickrun")
+      (spacemacs/set-leader-keys
+        "cqq" 'quickrun
+        "cqa" 'quickrun-with-arg
+        "cqs" 'quickrun-shell
+        "cqh" 'helm-quickrun))))
