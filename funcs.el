9d59382885f5afe3c33b652d84a34fa161f4ad23
Seong Yong-ju <sei40kr@gmail.com>
2018-04-18 00:11:54 +0900

    Improve quickrun behavior
    Add a helm source to select the .qrinput* files, and call it before quickrun
    runs.

--- funcs.el
+++ funcs.el
@@ -9,10 +9,32 @@
 ;;
 ;;; License: MIT
 
+(defun spacemacs/quickrun-may-ask-stdin ()
+  (let (
+         (stdin-files
+           (directory-files
+             default-directory
+             nil
+             (concat (quickrun--stdin-file-name) "*")))
+         new-stdin-file)
+    (when (<= 2 (length stdin-files))
+      (setq new-stdin-file
+        (save-window-excursion
+          (helm
+            :sources 'helm-source-files-in-current-dir
+            :input (concat
+                     quickrun--executed-file
+                     (default-value 'quickrun-input-file-extension)))))
+      (when new-stdin-file
+        (setq-local quickrun-input-file-extension
+          (concat "." (file-name-extension (car new-stdin-file))))))))
+
+
 ;; cf https://github.com/Codas/spacemacs-config/blob/master/quickrun/packages.el
 
 (defun spacemacs/quickrun ()
   (interactive)
+  (spacemacs/quickrun-may-ask-stdin)
   (if (eq evil-state 'visual)
     (quickrun-region (region-beginning) (region-end))
     (quickrun)))
